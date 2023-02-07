a0dd6be37fd83e6ca17253ed65366b320c880f02
SivakumarViyalan <v.sivakumar.mca@gmail.com>
2023-02-07 18:47:27 +0530

    Amdocs Python Day-1 Training

---
+++ Day01/sample.py
@@ -0,0 +1,17 @@
+#! c:\python\bin
+
+print("Hello World")
+
+def fun():
+ '''SIVAKUMAR VIYALAN'''
+ """AMDOCS"""
+ print("Hello world")
+ print("function code")
+ for i in range(1, 20):
+  if i % 2 == 1:
+   print(i)
+fun()
+
+print ("=" * 40) # THIS IS A COMMENT
+
+print(fun().__doc__)
